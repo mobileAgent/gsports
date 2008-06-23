bd7107e91894cdea683009bce9db8cb04ffe8a34
Mike Flester <mike@integratedcc.com>
2008-06-23 08:50:48 -0400

    set up for mysql and baseline plugins

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,10 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
+  # Turn on community engine routes
+  map.from_plugin :community_engine
+
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
