e34d3706b7fc458eb7c3b729c5a0ac514a98fe7a
mark <mark@peshawar.home>
2008-07-13 22:03:24 -0400

    Added relationship to subscription_plan

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,8 @@
 require 'vendor/plugins/community_engine/app/models/role'
 class Role < ActiveRecord::Base
+  TEAM_ROLE = "team"
+  LEAGUE_ROLE = "league"
+  SCOUT_ROLE = "scout"
+  
   belongs_to :subscription_plan
 end
