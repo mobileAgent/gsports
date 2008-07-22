b6fc2dcaac0a63ffad201a30778acfda41c692fb
Mike <mike@Psalm8.local>
2008-07-22 19:00:29 -0400

    make the home and visiting team relations save properly

--- app/models/video_reel.rb
+++ app/models/video_reel.rb
@@ -2,6 +2,10 @@ class VideoReel < ActiveRecord::Base
   
   belongs_to :user
   
+  acts_as_commentable
+  acts_as_taggable
+  belongs_to :favoritable, :polymorphic => true
+  
   # Every reel needs a title
   validates_presence_of :title
 end
