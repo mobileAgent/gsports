33e6d3274cd3a79b2a3ca4b964c76e5273c34840
Mike <mike@Psalm8.local>
2008-08-02 15:48:24 -0400

    failsave video asset for game of the week

--- app/models/game_of_the_week.rb
+++ app/models/game_of_the_week.rb
@@ -2,6 +2,11 @@ class GameOfTheWeek
   # This is a wrapper around the admin users favorite videos
   def self.find
     videos = Favorite.user(User.find_by_email(ADMIN_EMAIL)).videos(:order => "updated_at DESC", :limit => 6)
+    # Add a last resort to keep the whole site from being borked
+    if videos.size == 0
+      videos << VideoAsset.first 
+    end
+    
     videos.inject([]) do |list,v|
       begin
         case v.favoritable_type
