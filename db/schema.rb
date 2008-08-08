d9b6962fed6cbfe4c7c870cee1d59d4b97276de2
Mike <flester@gmail.com>
2008-08-08 09:22:57 -0400

    setting up categories for posts to support athlete of the week

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080808004000) do
+ActiveRecord::Schema.define(:version => 20080808121837) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id",    :limit => 10
