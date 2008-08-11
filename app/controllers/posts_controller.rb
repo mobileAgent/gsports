0bd539d2bdc71dc5071f3aa083743cdd84820bb4
Mike <flester@gmail.com>
2008-08-10 22:57:00 -0400

    show featured athlete posts to the public from the home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,16 @@ class PostsController < BaseController
   before_filter :login_required, :only => [:new, :edit, :update, :destroy, :create, :manage, :show, :popular]
   uses_tiny_mce(:options => AppConfig.gsdefault_mce_options, :only => [:new, :edit, :update, :create ])
   uses_tiny_mce(:options => AppConfig.simple_mce_options, :only => [:show])
+
+  # Allowed to show any of the featured athlete stories, but not just any story
+  def show_public
+    @post = AthleteOfTheWeek.find(params[:id])
+    unless @post.category_id == AthleteOfTheWeek.my_category.id &&
+        (@post.user.league_staff? || @post.user.team_staff? || @post.user.admin?)
+      redirect_to :controller => 'base', :action => 'site_index' and return
+    end
+  rescue
+    redirect_to :controller => 'base', :action => 'site_index'
+  end
   
 end
--- app/models/athlete_of_the_week.rb
+++ app/models/athlete_of_the_week.rb
@@ -5,26 +5,28 @@ class AthleteOfTheWeek < Post
 
   attr_accessor :photo_id
 
+  def self.my_category
+    Category.find_by_name(CATEGORY_NAME)
+  end
+
   # This is a wrapper around posts in a certain category. 
   # The athletes of the week for a league or team will be the
   # two most recent posts to that category by any member of the
   # respective team of league staff.
   def self.for_team(team_id)
-    category = Category.find_by_name(CATEGORY_NAME)
     team_staff_ids = User.team_staff(team_id).collect(&:id)
-    return [] if(category.nil? || team_staff_ids.empty?)
+    return [] if(my_category.nil? || team_staff_ids.empty?)
     AthleteOfTheWeek.find(:all, 
-                          :conditions => ["category_id = ? and user_id IN (?) ", category.id, team_staff_ids], 
+                          :conditions => ["category_id = ? and user_id IN (?) ", my_category.id, team_staff_ids], 
                           :order => "published_at DESC", 
                           :limit => 2)
   end
 
   def self.for_league(league_id)
-    category = Category.find_by_name(CATEGORY_NAME)
     league_staff_ids = User.league_staff(league_id).collect(&:id)
-    return [] if(category.nil? || league_staff_ids.empty?)
+    return [] if(my_category.nil? || league_staff_ids.empty?)
     AthleteOfTheWeek.find(:all, 
-                          :conditions => ["category_id = ? and user_id IN (?) ", category.id, league_staff_ids], 
+                          :conditions => ["category_id = ? and user_id IN (?) ", my_category.id, league_staff_ids], 
                           :order => "published_at DESC", 
                           :limit => 2)
   end
@@ -33,11 +35,10 @@ class AthleteOfTheWeek < Post
   # chosen randomly from among recent team and league
   # athletes of the week.
   def self.for_home_page
-    category = Category.find_by_name(CATEGORY_NAME)
     ids = User.find(:all, :conditions => ["role_id IN (?)",[Role[:league].id,Role[:league_staff].id,Role[:team].id,Role[:team_staff].id,Role[:admin].id]]).collect(&:id)
-    return [] if (category.nil? || ids.empty?)
+    return [] if (my_category.nil? || ids.empty?)
     posts = AthleteOfTheWeek.find(:all,
-                      :conditions => ["category_id = ? and user_id IN (?) and published_at > ?",category.id,ids,8.days.ago],
+                      :conditions => ["category_id = ? and user_id IN (?) and published_at > ?",my_category.id,ids,8.days.ago],
                       :order => "published_at DESC")
 
     # If we have none, 1 or 2, just let them go
