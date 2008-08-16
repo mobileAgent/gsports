aa4a996ef8c12d5d1d2b06441e3ae504820b50ec
Mike Flester <mike@psalm8.local>
2008-08-16 12:00:12 -0400

    ugly, but force our way to db:migrate from nothing, this smells

--- db/migrate/20080703151157_create_admin_user.rb
+++ db/migrate/20080703151157_create_admin_user.rb
@@ -2,7 +2,7 @@ require 'active_record/fixtures'
 
 class CreateAdminUser < ActiveRecord::Migration
   def self.up
-    execute "insert into users (email,activated_at,login,salt,crypted_password,description,firstname,lastname,login_slug,role_id,birthday,address1,city,phone,created_at,updated_at) values ('#{ADMIN_EMAIL}','#{Time.now.to_s :db}','gsadmin','abcdefsaltydog','#{User.encrypt("gsadmin123","abcdefsaltydog")}','Global Sports Site Administrator','Ad','Ministrator','never-use-this-string',#{Role[:admin].id},'#{25.years.ago.to_date.to_s :db}','1 Global Sports Way','Annapolis','301.555.1212','#{Time.now.to_s :db}','#{Time.now.to_s :db}')"
+    execute "insert into users (email,activated_at,login,salt,crypted_password,description,firstname,lastname,login_slug,role_id,birthday,address1,city,phone,created_at,updated_at) values ('#{ADMIN_EMAIL}','#{Time.now.to_s :db}','gsadmin','abcdefsaltydog','#{Digest::SHA1.hexdigest("--abcdefsaltydog--gsadmin123--")}','Global Sports Site Administrator','Ad','Ministrator','never-use-this-string',#{Role[:admin].id},'#{25.years.ago.to_date.to_s :db}','1 Global Sports Way','Annapolis','301.555.1212','#{Time.now.to_s :db}','#{Time.now.to_s :db}')"
   end
 
   def self.down
--- db/migrate/20080729115007_add_team_league_avatar.rb
+++ db/migrate/20080729115007_add_team_league_avatar.rb
@@ -5,39 +5,41 @@ class AddTeamLeagueAvatar < ActiveRecord::Migration
     add_column :teams, :avatar_id, :integer
     add_column :leagues, :avatar_id, :integer
 
-    puts "Creating the default team and league logo"
-    src_photo = "#{RAILS_ROOT}/public/images/gs_header_logo.png"
-    admin_user = User.find_by_email(ADMIN_EMAIL)
-    gs_logo = Photo.create(:name=>'Global Sports',:description=>'Global Sports',:content_type=>'image/png', :filename => src_photo)
-    gs_logo.user= admin_user
-    gs_logo.size= File.size(src_photo)
-    gs_logo.save!
-    dst_photo = "#{RAILS_ROOT}/public/photos/0000/"
-    dst_photo << sprintf("%04d",gs_logo.id)
-    File.makedirs dst_photo
-    dst_photo << "/gs_header_logo.png"
-    File.copy src_photo, dst_photo
+    #puts "Creating the default team and league logo"
+    #src_photo = "#{RAILS_ROOT}/public/images/gs_header_logo.png"
+    #admin_user = User.find_by_email(ADMIN_EMAIL)
+    #gs_logo = Photo.create(:name=>'Global Sports',:description=>'Global Sports',:content_type=>'image/png', :filename => src_photo)
+    #gs_logo.user= admin_user
+    #gs_logo.size= File.size(src_photo)
+    #gs_logo.save!
+    #dst_photo = "#{RAILS_ROOT}/public/photos/0000/"
+    #dst_photo << sprintf("%04d",gs_logo.id)
+    #File.makedirs dst_photo
+    #dst_photo << "/gs_header_logo.png"
+    #File.copy src_photo, dst_photo
     
-    puts "Ensure all teams have an avatar id"
-    Team.find(:all, :conditions => 'avatar_id IS NULL').each do |t|
-      t.avatar= gs_logo
-      t.save!
-    end
+    #puts "Ensure all teams have an avatar id"
+    #Team.find(:all, :conditions => 'avatar_id IS NULL').each do |t|
+    #  t.avatar= gs_logo
+    #  t.save!
+    #end
     
-    puts "Ensure all leagues have an avatar id"
-    League.find(:all, :conditions => 'avatar_id IS NULL').each do |l|
-      l.avatar= gs_logo
-      l.save!
-    end
+    #puts "Ensure all leagues have an avatar id"
+    #League.find(:all, :conditions => 'avatar_id IS NULL').each do |l|
+    #  l.avatar= gs_logo
+    #  l.save!
+    #end
     puts "Ensure all teams have a league"
-    Team.find(:all, :conditions => 'league_id IS NULL').each do |t|
-      t.update_attributes({:league_id => admin_user.league.id})
-    end
+    execute "update teams set league_id = 1 where league_id is null"
+    #Team.find(:all, :conditions => 'league_id IS NULL').each do |t|
+    #  t.update_attributes({:league_id => admin_user.league.id})
+    #end
 
     puts "Ensure all users have a team"
-    User.find(:all, :conditions => 'team_id IS NULL').each do |u|
-      u.update_attributes({:team_id => admin_user.team_id})
-    end
+    execute "update users set team_id = 1 where team_id is null"
+    #User.find(:all, :conditions => 'team_id IS NULL').each do |u|
+    #  u.update_attributes({:team_id => admin_user.team_id})
+    #end
   end
 
   def self.down
