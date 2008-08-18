7810b6761746300c214d431f3085133327e00c90
Aaron Barnett <aaron@sonic.(none)>
2008-08-18 13:32:36 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -512,8 +512,8 @@ ActiveRecord::Schema.define(:version => 20080817031840) do
     t.string   "video_length"
     t.string   "video_type"
     t.string   "video_status"
-    t.integer  "league_id",          :limit => 11
-    t.integer  "team_id",            :limit => 11
+    t.integer  "sponsor_id",         :limit => 11
+    t.integer  "member_id",          :limit => 11
     t.integer  "user_id",            :limit => 11
     t.string   "sport"
     t.datetime "game_date"
@@ -525,6 +525,8 @@ ActiveRecord::Schema.define(:version => 20080817031840) do
     t.string   "game_level"
     t.string   "game_gender"
     t.integer  "view_count",         :limit => 11, :default => 0
+    t.integer  "team_id",            :limit => 11
+    t.integer  "league_id",          :limit => 11
     t.boolean  "public_video",                     :default => true
   end
 
