2c6ed0a92cf8694e5ca50bf3272377deb7198b6d
Mike Flester <mike@psalm8.local>
2008-08-18 12:20:56 -0400

    let forums be useful to everyone, not just admins

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -4,4 +4,9 @@ class ForumsController < BaseController
   before_filter :admin_required , :only => [:create, :update, :destroy]
   uses_tiny_mce :options => AppConfig.gsdefault_mce_options
 
+  protected
+
+  def authorized?
+    current_user
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,11 @@
 class TopicsController < BaseController
   before_filter :login_required, :only => [:index, :show]
-  before_filter :admin_required, :only => [:create, :new, :update, :destroy]
+  before_filter :admin_required, :only => [:destroy]
   uses_tiny_mce(:options => AppConfig.gsdefault_mce_options, :only => [:show, :new])
+
+  protected
+  
+    def authorized?
+      current_user
+    end
 end
