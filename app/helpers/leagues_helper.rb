e95e6ea831432be174d78f50942d2444e7eaf0e4
Aaron Barnett <aaron@sonic.(none)>
2008-08-20 13:12:31 -0400

    dashboard news feeds restructuring

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -1,12 +1,19 @@
 module LeaguesHelper
 
-  def league_posts()
-    Post.find(:all,
+  def league_posts(league=nil, opts={})
+    league ||= @league
+    
+    options = {
       :joins=>"JOIN users ON user_id = users.id",
-      :conditions=>["users.league_id = ?", @league.id],
+      :conditions=>["users.league_id = ?", league.id],
       :limit=>10
-      )
+    }.merge opts
+    
+    posts = Post.find(:all, options)
+    
+    posts ||= []
   end
+  
   
   def league_videos()
     VideoAsset.for_league(@league).all(:limit => 10, :order => 'updated_at DESC')
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,15 +1,19 @@
 module TeamsHelper
   
-  def team_posts(team=nil)
+  def team_posts(team=nil, opts={})
     team ||= @team
-    posts = Post.find(:all,
-        :joins=>"JOIN users ON user_id = users.id",
-        :conditions=>["users.team_id = ?", team.id],
-        :limit=>10
-        )
+    
+    options = {
+      :joins=>"JOIN users ON user_id = users.id",
+      :conditions=>["users.team_id = ?", team.id],
+      :limit=>10
+    }.merge opts
+    
+    posts = Post.find(:all, options)
+    
     posts ||= []
-    posts
   end
+  
   
   def team_videos()
     VideoAsset.for_team(@team).all(:limit => 10, :order => 'updated_at DESC')
