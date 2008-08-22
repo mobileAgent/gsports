6ee4dbc79757fae4f2c77edf046b63e928ca6f47
Mike Flester <mike@psalm8.local>
2008-08-21 23:02:17 -0400

    Revert "Memberships now require an address, migration to update memberships"
    This reverts commit bf83f494face2d9bedf45742577527ceabf5b813.
    
    Signed-off-by: Mike Flester <mike@psalm8.local>

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,10 +5,69 @@ class Membership < ActiveRecord::Base
    has_many :membership_billing_histories, :order => "created_at DESC"
    has_one  :credit_card
 
-   validates_presence_of :address
-
   CREDIT_CARD_BILLING_METHOD = "cc"
   INVOICE_BILLING_METHOD = "invoice"
+
+   STATES = {
+  'AL' => 'Alabama',
+  'AK' => 'Alaska',
+  'AS' => 'America Samoa',
+  'AZ' => 'Arizona',
+  'AR' => 'Arkansas',
+  'CA' => 'California',
+  'CO' => 'Colorado',
+  'CT' => 'Connecticut',
+  'DE' => 'Delaware',
+  'DC' => 'District of Columbia',
+  'FM' => 'Micronesia1',
+  'FL' => 'Florida',
+  'GA' => 'Georgia',
+  'GU' => 'Guam',
+  'HI' => 'Hawaii',
+  'ID' => 'Idaho',
+  'IL' => 'Illinois',
+  'IN' => 'Indiana',
+  'IA' => 'Iowa',
+  'KS' => 'Kansas',
+  'KY' => 'Kentucky',
+  'LA' => 'Louisiana',
+  'ME' => 'Maine',
+  'MH' => 'Islands1',
+  'MD' => 'Maryland',
+  'MA' => 'Massachusetts',
+  'MI' => 'Michigan',
+  'MN' => 'Minnesota',
+  'MS' => 'Mississippi',
+  'MO' => 'Missouri',
+  'MT' => 'Montana',
+  'NE' => 'Nebraska',
+  'NV' => 'Nevada',
+  'NH' => 'New Hampshire',
+  'NJ' => 'New Jersey',
+  'NM' => 'New Mexico',
+  'NY' => 'New York',
+  'NC' => 'North Carolina',
+  'ND' => 'North Dakota',
+  'OH' => 'Ohio',
+  'OK' => 'Oklahoma',
+  'OR' => 'Oregon',
+  'PW' => 'Palau',
+  'PA' => 'Pennsylvania',
+  'PR' => 'Puerto Rico',
+  'RI' => 'Rhode Island',
+  'SC' => 'South Carolina',
+  'SD' => 'South Dakota',
+  'TN' => 'Tennessee',
+  'TX' => 'Texas',
+  'UT' => 'Utah',
+  'VT' => 'Vermont',
+  'VI' => 'Virgin Island',
+  'VA' => 'Virginia',
+  'WA' => 'Washington',
+  'WV' => 'West Virginia',
+  'WI' => 'Wisconsin',
+  'WY' => 'Wyoming'
+}
 
   def last_billed
     membership_billing_histories.first.created_at
