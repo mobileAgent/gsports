22232597b8c6f12b3600704f062cf06e5429e374
Mike Flester <mike@psalm8.local>
2008-09-04 08:40:58 -0400

    permissions for team and league staff to edit their respective team or league record

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -35,6 +35,10 @@ class ApplicationController < ActionController::Base
     return true
   end
 
+  def admin_for_league_or_team
+    (current_user.admin? || current_user.league_admin? || current_user.team_admin?) ? true : access_denied
+  end
+  
   # Allow these models to be memcached
   def preload_models
     Role
@@ -44,6 +48,7 @@ class ApplicationController < ActionController::Base
     League
     Moniker
     User
+    Favorite
     VideoAsset
     VideoClip
     VideoReel
