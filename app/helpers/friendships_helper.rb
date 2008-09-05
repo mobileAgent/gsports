ae96391bf64fb8a34ea031ae836b4bb1137bbf35
Mike Flester <mike@psalm8.local>
2008-09-04 23:06:14 -0400

    resolve issues with navigating chain of friendships, reconnect friendship request button to ajax action

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,25 +1,33 @@
 module FriendshipsHelper
 
-  def friendship_control_links(friendship,current_user)
+  def friendship_control_links(friendship,owner,current_user)
     links = []
-    if friendship.user.id == current_user.id
-      case friendship.friendship_status_id
-      when FriendshipStatus[:pending].id
-        if !friendship.initiator?
-          links << ['Accept', accept_user_friendship_path(friendship.user, friendship), {:method => :put, :class => 'button positive genericButton'} ] #unless friendship.initiator?
+    posButtonOpts = {:method => :put, :class => 'button positive genericButton'}
+    negButtonOpts = {:method => :put, :class => 'button negative genericButton'}
+    if (current_user.id == owner.id)
+      if friendship.user_id == current_user.id
+        case friendship.friendship_status_id
+        when FriendshipStatus[:pending].id
+          if !friendship.initiator?
+            links << ['Accept', accept_user_friendship_path(friendship.user, friendship), posButtonOpts ]
+          end
+          links << ['Deny', deny_user_friendship_path(friendship.user, friendship), negButtonOpts ]
+        when FriendshipStatus[:accepted].id  
+          links << ['Send Message', new_message_path(:to => friendship.friend.id), posButtonOpts ]
+          links << ["Remove this friend", deny_user_friendship_path(friendship.user, friendship), negButtonOpts ]
+        when FriendshipStatus[:denied].id
+          links << ["Accept this request", accept_user_friendship_path(friendship.user, friendship), posButtonOpts ]
         end
-        links << ['Deny', deny_user_friendship_path(friendship.user, friendship), {:method => :put, :class => 'button negative genericButton'} ]
-      when FriendshipStatus[:accepted].id  
-        links << ['Send Message', new_message_path(:to => friendship.friend.id), {:class => 'genericButton'} ]
-        links << ["Remove this friend", deny_user_friendship_path(friendship.user, friendship), {:method => :put, :class => 'button negative genericButton'} ]
-        if (friendship.friend.accepted_friendships.size > 1)
-          links << [ "See #{friendship.friend.accepted_friendships.size} Friends", accepted_user_friendships_path(friendship.friend), {:class => 'genericButton'}]
-        end
-      when FriendshipStatus[:denied].id
-        links << ["Accept this request", accept_user_friendship_path(friendship.user, friendship), {:method => :put, :class => 'button positive genericButton'} ]
       end
     elsif friendship.friend_id != current_user.id
-      links << ['Request Friendship',url_for({ :controller => 'friendships', :action => 'create', :user_id => current_user.id, :friend_id => friendship.friend_id}), {:method => :post, :class => 'genericButton'}]
+      if current_user.friendships.collect(&:friend_id).member?(friendship.friend_id)
+        if (current_user.accepted_friendships.collect(&:friend_id).member?(friendship.friend_id))
+            links << ['Send Message', new_message_path(:to => friendship.friend.id), posButtonOpts ]
+        end
+      end
+    end
+    if (friendship.friend.accepted_friendships.size > 1)
+      links << [ "See #{friendship.friend.accepted_friendships.size} Friends", accepted_user_friendships_path(friendship.friend), {:class => 'genericButton'}]
     end
     links
   end
