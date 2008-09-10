58702752c380ba0333b4fe48df25fde3a588bd15
Mike Flester <flester@gmail.com>
2008-09-10 10:34:36 -0400

    schema to go with last migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080908154511) do
+ActiveRecord::Schema.define(:version => 20080910141520) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id",    :limit => 10
