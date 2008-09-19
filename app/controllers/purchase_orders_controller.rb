bca39580963b620afc2b06057e5142d7b39170bd
Aaron Barnett <aaron@sonic.(none)>
2008-09-19 09:15:29 -0400

    moved PO to its own controller, preparing for another nasty merge

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,42 +1,41 @@
 class PurchaseOrdersController < BaseController
 
+  before_filter :admin_required, :only => [:index]
   skip_before_filter :gs_login_required, :only => [:show, :new, :confirm, :create]
 
 
-  
-  def poshow
-    @user = User.find(params[:userid].to_i)
-    @item_no = @user.role_id
-    if @user.team_staff?
-      @entity = @user.team 
-      @description = "Monthly Member Firm Subscription"
-    else #if @user.league_staff?
-      @entity = @user.league
-      @description = "Monthly Sponsor Organiztion Subscription"
-    end
-    timestamp = Time.now.to_i
-    @invoice_no = "#{@user.id}-#{timestamp}"
-    render :layout => false
+  def index
+    @pos = PurchaseOrder.find(:all)
   end
   
   def new
     @po = PurchaseOrder.new(params[:purchase_order])
-    render :layout => false
+    
+    @promotion = session[:promotion]
+    @cost = (@promotion && @promotion.cost) ? @promotion.cost : @po.user.role.plan.cost
+    
   end  
   
-  def confirm
+  def xconfirm
     @po = PurchaseOrder.new(params[:purchase_order])
-    if @po.valid?
-      render :layout => false
-    else  
-      render :action=>:new, :layout => false
+    if !@po.valid?
+      render :action=>:new
     end
   end
   
   def create
     @po = PurchaseOrder.new(params[:purchase_order])
-    @po.save!
-    render :action=>:show, :layout => false, :id=>@po.id
+    if params[:confirm]
+      raise(RecordNotSaved)
+      @po.save!
+      render :action=>:show, :layout => false, :id=>@po.id
+    else
+      raise(RecordNotSaved) if !@po.valid?
+      render :action=>:confirm
+    end
+    
+  rescue ActiveRecord::RecordInvalid => e
+    render :action => 'new'
   end
   
   def show
