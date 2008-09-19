9dcf1a0d79b05c4dda2c2ad33c08adc8acb0a9e5
Mike Flester <flester@gmail.com>
2008-09-19 16:44:11 -0400

    comment out an obsolete query, use includes on another

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < BaseController
     end
     
     @friend_count = @user.accepted_friendships.count
-    @accepted_friendships = @user.accepted_friendships.find(:all, :limit => 5).collect{|f| f.friend }
+    @accepted_friendships = @user.accepted_friendships.find(:all, :limit => 5, :include => [:friend]).collect{|f| f.friend }
     @pending_friendships_count = @user.pending_friendships.count()
 
     fav_cond = { :user_id => @user.id }
@@ -35,10 +35,10 @@ class UsersController < BaseController
     @comments = @user.comments.find(:all, :limit => 10, :order => 'created_at DESC')
     @photo_comments = Comment.find_photo_comments_for(@user)
 
-    @users_comments = Comment.find_comments_by_user(@user, :limit => 5)
+    # @users_comments = Comment.find_comments_by_user(@user, :limit => 5)
 
     @recent_posts = @user.posts.find(:all, :limit => 2, :order => "published_at DESC")
-    @clippings = @user.clippings.find(:all, :limit => 5)
+    # @clippings = @user.clippings.find(:all, :limit => 5)
     @photos = @user.photos.find(:all, :limit => 5)
     @comment = Comment.new(params[:comment])
     @published_post_count = Post.count(:all, :conditions => ["user_id = ? and published_as = ?", @user.id, 'live'])
