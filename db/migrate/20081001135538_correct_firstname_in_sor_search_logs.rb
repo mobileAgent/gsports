29ccfef8a3865706f99c5b8ffa48f2344b493dd5
John Bresnik <jbresnik@gmail.com>
2008-10-03 00:41:46 -0700

    Updates for SOR - finished states

---
+++ db/migrate/20081001135538_correct_firstname_in_sor_search_logs.rb
@@ -0,0 +1,10 @@
+class CorrectFirstnameInSorSearchLogs < ActiveRecord::Migration
+  def self.up    
+      rename_column :sor_search_logs, :fisrtname, :firstname
+    end
+  end
+
+  def self.down    
+    rename_column :sor_search_logs, :firstname, :fisrtname
+  end
+end
