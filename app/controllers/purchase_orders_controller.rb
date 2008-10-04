8ef2ba7c298279822027f09aa6cdf99859d52ab5
Jeremy Eckman <jeremy@localhost.localdomain>
2008-10-03 21:56:19 -0400

    added security to prevent users from displaying other purchase orders. Also prevent multiple purchase order creations when the print button is pressed

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,6 +1,6 @@
 class PurchaseOrdersController < BaseController
 
-  before_filter :admin_required, :only => [:index]
+  before_filter :admin_required, :only => [:index, :activate]
   skip_before_filter :gs_login_required, :only => [:show, :new, :confirm, :create]
   skip_before_filter :billing_required, :only => [:show, :new, :confirm, :create]  
 
@@ -9,6 +9,7 @@ class PurchaseOrdersController < BaseController
   end
   
   def new
+    session[:purchase_order] = nil
     @po = PurchaseOrder.new(params[:purchase_order])
     @promotion = session[:promotion]
     
@@ -18,51 +19,77 @@ class PurchaseOrdersController < BaseController
   end
   
   def create
-    @po = PurchaseOrder.new(params[:purchase_order])
-    @promotion = session[:promotion]
-    
-    # Purchase orders are made for the full retail price
-    @cost = @po.user.role.plan.cost
-    #@cost = (@promotion && !@promotion.cost.nil?) ? @promotion.cost : @po.user.role.plan.cost
-    
-    if params[:confirm] == 'yes'
-      if !params[:tos] || !params[:suba]
-        @po.errors.add('', "Please accept the Terms of Service and the Subscriber Agreement") 
-        render :action=>:confirm
-      else
-        
-        @po.due_date = Time.now + 2.weeks
-        if @promotion && @promotion.period_days
-          @po.due_date += @promotion.period_days.days
+    # Catch the case when the user clicks the Print button multiple times
+    if current_user.nil? && session[:purchase_order]
+      logger.debug "Got Purchase Order off the session"
+      @po = session[:purchase_order]
+      @promotion = session[:promotion]
+      redirect_to :action=>:show, :layout => false
+    else
+      @po = PurchaseOrder.new(params[:purchase_order])
+      @promotion = session[:promotion]
+      
+      
+      if params[:confirm] == 'yes'
+        if !params[:tos] || !params[:suba]
+          @po.errors.add('', "Please accept the Terms of Service and the Subscriber Agreement") 
+          
+          # Purchase orders are made for the full retail price
+          @cost = @po.user.role.plan.cost
+          
+          render :action=>:confirm
+        else
+          
+          @po.due_date = Time.now + 2.weeks
+          if @promotion && @promotion.period_days
+            @po.due_date += @promotion.period_days.days
+          end
+          
+          @po.save!
+          
+          # Add the membership record here
+          @po.user.make_member_by_invoice(@cost,@po,@promotion)
+  
+          #########################
+          # If we have to auto-enable users when a po is submitted, here is how
+          # @po.user.enabled = true
+          # @po.user.activated_at = Time.now
+          # @po.user.save!
+          # self.current_user = @po.user # Log them in right now!
+          # UserNotifier.deliver_welcome(@po.user)
+          #########################
+          
+          # Put the po on the session, otherwise this user has no access to it
+          # Also, it is used to catch the case when the user 
+          # clicks the Print button multiple times
+          session[:purchase_order] = @po
+          
+          redirect_to :action=>:show, :layout => false
         end
-        
-        @po.save!
-        
-        # Add the membership record here
-        @po.user.make_member_by_invoice(@cost,@po,@promotion)
-
-        #########################
-        # If we have to auto-enable users when a po is submitted, here is how
-        # @po.user.enabled = true
-        # @po.user.activated_at = Time.now
-        # @po.user.save!
-        # self.current_user = @po.user # Log them in right now!
-        # UserNotifier.deliver_welcome(@po.user)
-        #########################
-        
-        redirect_to :action=>:show, :layout => false, :id=>@po.id
+      else    
+        raise(ActiveRecord::RecordInvalid.new(@po)) if !@po.valid?
+        render :action=>:confirm
       end
-    else    
-      raise(ActiveRecord::RecordInvalid.new(@po)) if !@po.valid?
-      render :action=>:confirm
-    end
-    
+    end    
   rescue ActiveRecord::RecordInvalid => e  
     render :action => 'new'
   end
   
   def show
-    @po = PurchaseOrder.find(params[:id].to_i)
+    if current_user.nil?
+      @po = session[:purchase_order]
+    else
+      @po = PurchaseOrder.find(params[:id].to_i)
+  
+      if @po
+        unless current_user.admin? || current_user.id == @po.user_id
+          render :action => 'private'
+        end
+      end
+    end
+    
+    @promotion = @po.membership.promotion
+    
     render :layout => false
   end
   
