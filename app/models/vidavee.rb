d771151bb22e4d6d8b90ed545a4d9e584831c756
Mike Flester <flester@gmail.com>
2008-11-05 16:47:36 -0500

    switch vidavee interaction from net http to curb/curl for memory and streaming large file uploads #165

--- app/models/vidavee.rb
+++ app/models/vidavee.rb
@@ -18,29 +18,23 @@ class Vidavee < ActiveRecord::Base
     ["*.3g2","*.3gp","*.3gp2","*.3gpp","*.asf","*.avi","*.avs","*.dv","*.flc","*.fli","*.flv","*.gvi","*.m1v","*.m2v","*.m4e","*.m4u","*.m4v","*.mjp","*.mkv","*.moov","*.mov","*.movie","*.mp4","*.mpe","*.mpeg","*.mpg","*.mpv2","*.qt","*.rm","*.ts","*.vfw","*.vob","*.wm","*.wmv"]
   end
 
-  # Our HTTP Client to communicate with Vidavee service
-  CLIENT = HTTPClient.new
-  CLIENT.connect_timeout = 10
-  CLIENT.receive_timeout = 30
-  CLIENT.send_timeout = 30
-  LOGIN_TIMEOUT = 3
-  
-  # Turn this on for debug of HTTP traffic to Vidavee
-  # CLIENT.debug_dev = STDERR
-
   # http://tribeca.vidavee.com/hsstv/rest/session/CheckUser;jsessionid=39555E57BCF38625CF7DEA2EDD9038F7.node1?api_key=5342smallworld&api_ts=1214532647018&api_token=39555E57BCF38625CF7DEA2EDD9038F7.node1&api_sig=830678DF3E967D0392F936122F767754&session_id=
   # Seems to always return false, not sure what good it is
   def check_user(sessionid)
     response = vrequest('session/CheckUser',sessionid)
-    extract(response.content, '//isUserLoggedIn').text
+    if response
+      extract(response, '//isUserLoggedIn').text
+    else
+      nil
+    end
   end
 
   # http://tribeca.vidavee.com/hsstv/rest/session/Login?api_key=5342smallworld&api_ts=1214531598256&api_sig=EFF68F8B9CBC146FCEE39EA3D4AFED79&userName=hsstv&password=hsstvUser
   def login
     response = vrequest('session/Login')
-    if response && response.content
+    if response
       logger.debug "Logging into the Vidavee backend"
-      extract(response.content,'//newToken').text;
+      extract(response,'//newToken').text;
     else
       nil
     end
@@ -49,35 +43,34 @@ class Vidavee < ActiveRecord::Base
   # Logout of the session
   def logout(sessionid)
     response = vrequest('session/Logout')
-    response.status == 200
+    #response.status == 200
   end
 
   def customer_groups(sessionid)
     response = vrequest('customers/GetCustomerGroups',sessionid)
-    extract(response.content,'//group')
+    if response
+      extract(response,'//group')
+    end
   end
 
   def delete_content(sessionid,dockey)
     response = vrequest('assets/DeleteAsset',sessionid,DOCKEY_PARAM => dockey)
-    'ok' == extract(response.content,'/response')['status']
+    'ok' == extract(response,'/response')['status']
   end
 
   # Returns the thumbnail jpeg bytes for the dockey
   def thumbnail_bytes(sessionid,dockey)
-    response = vrequest('file/GetFileThumbnail',sessionid,DOCKEY_PARAM => dockey)
-    response.content
+    vrequest('file/GetFileThumbnail',sessionid,DOCKEY_PARAM => dockey)
   end
 
   # Returns the thumbnail jpeg bytes for the dockey, low res version
   def thumbnail_low_bytes(sessionid,dockey)
-    response = vrequest('file/GetFileThumbnailLow',sessionid,DOCKEY_PARAM => dockey)
-    response.content
+    vrequest('file/GetFileThumbnailLow',sessionid,DOCKEY_PARAM => dockey)
   end
   
   # Returns the thumbnail jpeg bytes for the dockey, medium res version
   def thumbnail_bytes(sessionid,dockey)
-    response = vrequest('file/GetFileThumbnailMedium',sessionid,DOCKEY_PARAM => dockey)
-    response.content
+    vrequest('file/GetFileThumbnailMedium',sessionid,DOCKEY_PARAM => dockey)
   end
 
   # Returns the thumbnail url for the dockey
@@ -109,13 +102,12 @@ class Vidavee < ActiveRecord::Base
   # (Note there is no http in front -- weird.)
   def file_flv(sessionid,dockey)
     url = "http://#{uri}/#{context}/vClientXML.view?AF_renderParam_contentType=text/xml&#{DOCKEY_PARAM}=#{dockey}"
-    response = CLIENT.post url
-    video_url = extract_no_status(response.content,"//url[@type='stream']/").first
+    response = Curl::Easy.http_post(url)
+    video_url = extract_no_status(response.body_str,"//url[@type='stream']/").first
     if video_url.nil?
       logger.debug "Found no valid url objects in response"
       return nil
     end
-    logger.debug "Video url is #{video_url.class} methods #{(video_url.methods.sort - Object.methods).join("\n")}"
     "http://#{video_url.inner_text}"
   end
 
@@ -131,7 +123,7 @@ class Vidavee < ActiveRecord::Base
   # Use param AF_page to see other than the first page
   def gallery_playlists(sessionid, extra_params = {})
     response = vrequest('playlist/GetGalleryPlaylists',sessionid,extra_params)
-    extract(response.content,'//asset')
+    extract(response,'//asset')
   end
 
   # List items in gallery, result is paginated, see currentPage, totalPages
@@ -145,13 +137,23 @@ class Vidavee < ActiveRecord::Base
     # Send the post
     form_params = { 'assetType' => asset_type, 'dateRange' => 'All' }
     begin
-      response = CLIENT.post(url, form_params)
-      assets = extract(response.content,'//asset')
+      response = curl_post(url,form_params)
+      assets = extract(response,'//asset')
     rescue TimeoutError
-      logger.error "Could not contact Vidavee backend"
+      logger.error "Could not contact Vidavee backend: #{$!}"
     end
     logger.debug "Asking for #{asset_type} with #{url} got #{assets.size}"
     assets
+  end
+
+  def curl_post(url,form_params={})
+    curl_params=form_params.inject([]) {|s,(k,v)| s << Curl::PostField.content(k,v)}
+    r = Curl::Easy.http_post(url,*curl_params)
+    if r.response_code == 200
+      return r.body_str
+    else
+      "There was an error #{r.response_code} from #{url}"
+    end
   end
 
   # Get all the vtag records for a video_asset
@@ -160,7 +162,7 @@ class Vidavee < ActiveRecord::Base
     extra_params[DOCKEY_PARAM] = dockey
     response = vrequest('gallery/GetGalleryVtagsByAsset',sessionid,extra_params,false)
     if (response)
-      extract(response.content,'//vtag')
+      extract(response,'//vtag')
     else
       nil
     end
@@ -169,9 +171,9 @@ class Vidavee < ActiveRecord::Base
   # Get details for a reel (vidavee calls this a playlist)
   def reel_details(sessionid, dockey)
     url = "http://#{uri}/#{context}/pClientXML.view?AF_renderParam_contentType=text/xml&#{DOCKEY_PARAM}=#{dockey}"
-    response = CLIENT.post url
-    response.content.gsub!('&dockey=','&amp;dockey=') if response.content
-    h = Hpricot.XML(response.content)
+    response = curl_post url
+    response.gsub!('&dockey=','&amp;dockey=') if response
+    h = Hpricot.XML(response)
     vpel = '/VVPlaylist'
     title = h.search(vpel).attr('title')
     title.gsub!(/:uid=\d+/,'') # cruft from the pilot site
@@ -245,12 +247,12 @@ class Vidavee < ActiveRecord::Base
   
   def update_asset_record(sessionid,video_asset,only={})
     response = vrequest('assets/GetDetailsAssetContent',sessionid, { DOCKEY_PARAM => video_asset.dockey })
-    if response && response.content
-      body = extract(response.content,"//content")
+    if response
+      body = extract(response,"//content")
       if body
         update_asset_record_from_xml(video_asset,body,only)
       else
-        logger.debug "Bad response #{response.content}"
+        logger.debug "Bad response #{response}"
       end
     else
       logger.debug "No content in response #{response}"
@@ -302,26 +304,26 @@ class Vidavee < ActiveRecord::Base
                         'width'=>width,'height'=>height,
                         'shareWidgets'=>'off',
                         'autoplay'=>autoplay, 'vtagView'=>vtag)
-    extract(response.content,'//assetEmbedCode').text
+    extract(response,'//assetEmbedCode').text
   end
 
   # This moves the entire asset to the trash, it should be deleted from there
   def delete_asset(sessionid, dockey)
     response = vrequest('assets/DeleteAsset',sessionid,DOCKEY_PARAM => dockey)
-    extract(response.content,'//status')
+    extract(response,'//status')
   end
 
   # looking for 'ready', 'queued', 'blocked'
   def asset_status(sessionid, dockey)
     response = vrequest('assets/GetDetailsAssetStatus',sessionid,DOCKEY_PARAM => dockey)
-    el = extract(response.content,'//status')
+    el = extract(response,'//status')
     el.text if el 
   end
 
   # zero or more asset tags
   def asset_tags(sessionid,dockey)
     response = vrequest('assets/GetDetailsAssetTags',sessionid,DOCKEY_PARAM => dockey)
-    extract(response.content,'//tags')
+    extract(response,'//tags')
   end
 
   # Gets info for the media in dockey, such as
@@ -330,13 +332,13 @@ class Vidavee < ActiveRecord::Base
   # audioChannels, audioBitrate, ...
   def media_info(sessionid, dockey)
     response = vrequest('assets/GetMediaInfo',sessionid,DOCKEY_PARAM => dockey)
-    extract(response.content,'//mediaInfo')
+    extract(response,'//mediaInfo')
   end
 
   # Owner is from what is listed in getCustomerGroups (I think)
   def sets(sessionid, owner)
     response = vrequest('users/GetSets',sessionid,'setOwner' => owner)
-    extract(response.content,'//set')
+    extract(response,'//set')
   end
 
   # Called from the activemessaging processor once we get the video
@@ -346,43 +348,29 @@ class Vidavee < ActiveRecord::Base
   # video_asset dockey and video_status are updated and saved on success
   def push_video(sessionid, video_asset, file_path)
 
-    # Build the url, yeah, with parameters on it
+    # Build the url, yeah, with parameter on it
     url = url_for('assets/NewAssetVideo',sessionid)
     params = build_request_params('assets/NewAssetVideo',sessionid)
     url = query_url(url,params)
     
-    # Convince HttpClient to post multipart data by sending a boundary
-    boundary = Array::new(8){ "%2.2d" % rand(99) }.join('__')
-    extheader = {'content-type' => "multipart/form-data; boundary=--------#{ boundary }__xyzzy"}
-
-    # Name and open the files
-    video_file = open(file_path)
-    upload_params = {'Asset' => video_file }
-    
     # Send in any extra parameters the upload form requires
-    upload_params['title'] = video_asset.title
-    upload_params['description'] = video_asset.description
-    upload_params['transcoderVersion'] = 3
-    upload_params['type'] = 'video'
     asis=file_path.downcase.end_with? '.flv'
-    upload_params['asisFlv'] = asis
-
+    upload_params = [
+                     Curl::PostField.content('title',video_asset.title),
+                     Curl::PostField.content('description',video_asset.description),
+                     Curl::PostField.content('transcoderVersion','3'),
+                     Curl::PostField.content('type','video'),
+                     Curl::PostField.content('asisFlv',asis.to_s),
+                     Curl::PostField.file('Asset',file_path)
+                     ]
     # Send the post
     begin
-      CLIENT.connect_timeout = 60
-      CLIENT.receive_timeout = 500
-      CLIENT.send_timeout = 0
-      response = CLIENT.post(url, upload_params, extheader)
-      CLIENT.connect_timeout = 30
-      CLIENT.receive_timeout = 30
-      CLIENT.send_timeout = 30
-    rescue TimeoutError
-      logger.error "Could not contact Vidavee backend"
-      response = "Timeout"
-    ensure
-      video_file.close
+      response = do_upload(url,*upload_params)
+    rescue 
+      logger.error "Could not contact Vidavee backend: #{$!}"
+      response = "Error"
     end
-    dockey_elem = extract(response.content,'//dockey')
+    dockey_elem = extract(response,'//dockey')
 
     # update attributes in the asset
     if dockey_elem
@@ -393,9 +381,20 @@ class Vidavee < ActiveRecord::Base
     else
       video_asset.video_status= 'upload failed'
       video_asset.save!
-      logger.debug "Video push failed: #{response.content}"
+      logger.debug "Video push failed: #{response}"
       nil
     end
+  end
+
+  def do_upload(url,*args)
+    c = Curl::Easy.new(url)
+    c.multipart_form_post= true
+    c.connect_timeout= 60
+    c.timeout= 0
+    logger.debug "Ready to upload to #{url} with #{args.length} args"
+    c.http_post(*args)
+    logger.debug "Done with upload, response #{c.response_code}"
+    c.body_str
   end
 
   #### Class methods
@@ -467,7 +466,12 @@ class Vidavee < ActiveRecord::Base
 
   # Build a query url from the base url given plus the query parameters
   def query_url(url, params)
-    url +"?" + params.inject([]) { |s,(k,v)| s << "#{k}=#{v}" }.join("&")
+    sep = "?"
+    params.each do |pf|
+      url = "#{url}#{sep}#{pf.name}=#{pf.content}"
+      sep = "&"
+    end
+    url
   end
 
   # extract some element from the response document
@@ -508,9 +512,16 @@ class Vidavee < ActiveRecord::Base
     url = url_for(action,sessionid)
     params = build_request_params(action,sessionid,extra_params,login)
     begin
-      response = CLIENT.post(query_url(url,params))
+      full_url = query_url(url,params)
+      response = Curl::Easy.http_post(full_url)
+      if (response.response_code == 200)
+        return response.body_str
+      else
+        logger.error "Vidavee response code #{response.response_code} on #{full_url} => #{response.body_str}"
+        return nil
+      end
     rescue 
-      logger.error "Could not contact Vidavee backend for #{url}"
+      logger.error "Could not contact Vidavee backend for #{url}: #{$!}"
       nil
     end
   end
@@ -519,16 +530,18 @@ class Vidavee < ActiveRecord::Base
   def build_request_params(action,sessionid='',extra_params={}, include_login=true)
     # get a timestamp in vidavee format for use in the http api
     ts = "%.0f" % (Time.now.to_f * 1000)
-    params = {KEY_PARAM => key,
-      TS_PARAM => ts,
-      SIG_PARAM => sign(action,ts),
-    }
+    params = []
+    params << Curl::PostField.content(KEY_PARAM,key)
+    params << Curl::PostField.content(TS_PARAM,ts)
+    params << Curl::PostField.content(SIG_PARAM,sign(action,ts))
     if include_login
-      params[USER_PARAM] = username
-      params[PASS_PARAM] = password
+      params << Curl::PostField.content(USER_PARAM,username)
+      params << Curl::PostField.content(PASS_PARAM,password)
     end
     if extra_params && extra_params.class == Hash
-      extra_params.each { |p| params[p[0]] = p[1] }
+      extra_params.each do |k,v|
+        params << Curl::PostField.content(k,"#{v}")
+      end
     end
     params
   end
