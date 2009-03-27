a201265a030c43a23f1a498c273c172b3315136f
Aaron Barnett <aaron@outerbody.com>
2009-03-27 11:35:18 -0400

    grant user access to multiple groups #282

--- app/models/access_user.rb
+++ app/models/access_user.rb
@@ -2,14 +2,8 @@ class AccessUser < ActiveRecord::Base
 
   belongs_to :access_group
   belongs_to :user
-
-  def x_validate
-    unless user.team_id == access_group.team_id
-      errors.add(:thumb_span, "User is not a member of the team that owns this Access Group.")
-    end
-  end  
-   
-   
+  
+  validates_uniqueness_of :user_id, :scope => [:access_group_id], :message => 'has already been added to this group.'
 
   def self.access_for user
     access = nil
