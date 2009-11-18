093df651a68a34ed86ec08854a89a0beab8aa8b9
Aaron Barnett <aaron@outerbody.com>
2009-11-17 22:38:49 -0500

    default permissions update

--- db/migrate/20091027210850_add_default_permissions.rb
+++ db/migrate/20091027210850_add_default_permissions.rb
@@ -1,13 +1,40 @@
 class AddDefaultPermissions < ActiveRecord::Migration
   def self.up
-    User.find(:all, :conditions=>['role_id in (?)', [Role[:team], Role[:league], Role[:team_staff], Role[:league_staff]] ]).each() { |user|
-      Permission.staff_permission_list.each() { |permission, name|
+    admin_perms= [
+      Permission::EDIT_TEAM_PAGE, 
+      Permission::MANAGE_CHANNELS,
+      Permission::MANAGE_GROUPS, 
+      Permission::CREATE_STAFF, 
+      Permission::UPLOAD
+    ]
+  
+    User.find(:all, :conditions=>['role_id in (?)', [Role[:team], Role[:league]] ]).each() { |user|
+      admin_perms.each() { |permission|
         begin
           scope = user.team_staff? ? user.team : user.league
           next if scope.nil?
           Permission.grant(user, permission, scope)
         rescue Exception=>e
-          puts "error granting permission for user:#{user}. #{e.message}"
+          puts "error granting permission for user:#{user.id}. #{e.message}"
+        end
+      }
+    }
+  
+    staff_perms = [
+      Permission::EDIT_TEAM_PAGE, 
+      Permission::MANAGE_CHANNELS,
+      Permission::MANAGE_GROUPS, 
+      Permission::UPLOAD
+    ]
+  
+    User.find(:all, :conditions=>['role_id in (?)', [Role[:team_staff], Role[:league_staff]] ]).each() { |user|
+      staff_perms.each() { |permission|
+        begin
+          scope = user.team_staff? ? user.team : user.league
+          next if scope.nil?
+          Permission.grant(user, permission, scope)
+        rescue Exception=>e
+          puts "error granting permission for user:#{user.id}. #{e.message}"
         end
       }
     }
